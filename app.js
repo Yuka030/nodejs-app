d47564b90c9c92e6bfc830c4aa5768969aa92e9d
Yuka <yuka.0531.a@gmail.com>
2021-03-01 20:18:00 -0800

    Send back form to a request to /add-product

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require("express");
 const app = express();
 
 app.use("/app-product", (req, res, next) => {
-  console.log("In the middleware");
-  res.send("page1");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body); //By default, request does not parse the incoming request body
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware");
   res.send("page2");
 });
 
