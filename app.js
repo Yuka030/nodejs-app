78229dc61fc8199ff9533f44a5282d345433121f
Yuka <yuka.0531.a@gmail.com>
2021-03-01 20:28:48 -0800

    Add body-parser to parse incoming request body

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded());
 
 app.use("/app-product", (req, res, next) => {
   res.send(
