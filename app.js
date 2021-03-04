bf33c54711fe76df8f60e99a1d25ba5fd890aee7
Yuka <yuka.0531.a@gmail.com>
2021-03-03 21:20:21 -0800

    Move routing related code into routes folder

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.urlencoded());
 
-app.use("/app-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></button></form>'
-  );
-});
+app.use(adminRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body); //By default, request does not parse the incoming request body
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("page2");
-});
+app.use(shopRoutes);
 
 app.listen(3000);
